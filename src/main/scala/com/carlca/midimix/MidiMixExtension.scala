3fa478aaff0266285f0dff9cb7ff00545104eaa0
Carl Caulkett <carl.caulkett@gmail.com>
2025-03-04 20:13:06 +0000

    MidiMix and Twisterella - a two state solution.

--- src/main/scala/com/carlca/midimix/MidiMixExtension.scala
+++ src/main/scala/com/carlca/midimix/MidiMixExtension.scala
@@ -5,10 +5,13 @@ import com.bitwig.extension.api.util.midi.ShortMidiMessage
 import com.bitwig.extension.controller.ControllerExtension
 import com.bitwig.extension.controller.api.*
 import com.carlca.config.Config
+import com.carlca.logger.Log
 
 class MidiMixExtension(definition: MidiMixExtensionDefinition, host: ControllerHost)
     extends ControllerExtension(definition, host):
-  private val APP_NAME                    = "com.carlca.MidiMix"
+  private val APP_NAME = "com.carlca.MidiMix"
+  private val STATUS_RANGE = ShortMidiMessage.CONTROL_CHANGE to (ShortMidiMessage.CONTROL_CHANGE + 15)
+  private val CC_RANGE = 16 to 255
 
   override def init: Unit =
     val host = getHost
@@ -33,9 +36,18 @@ class MidiMixExtension(definition: MidiMixExtensionDefinition, host: ControllerH
   private def initOnSysexCallback(host: ControllerHost): Unit =
     host.getMidiInPort(0).setSysexCallback(onSysex0(_))
 
+  private def unpackMsg(msg: ShortMidiMessage, doLog: Boolean = false): (Int, Int, Int, Int) =
+    val status = msg.getStatusByte()
+    val channel = status & 0x0F
+    val cc = msg.getData1()
+    val data2 = msg.getData2()
+    if doLog then Log.send(s"unpackMsg: status=$status, channel=$channel, cc=$cc, data2=$data2")
+    (status, channel, cc, data2)
+
   private def onMidi0(msg: ShortMidiMessage): Unit =
-    MidiProcessor.process(msg)
-  end onMidi0
+    val (status, channel, cc, data2) = unpackMsg(msg, true)
+    if (STATUS_RANGE contains status) && (CC_RANGE contains cc) then
+      MidiProcessor.process(msg)
 
   @FunctionalInterface
   private def onSysex0(data: String): Unit = ()
